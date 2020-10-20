483b8ba6352dd30bb023ddf17d97a1d38492ad75
suyeon <66218824+suyeon-DV@users.noreply.github.com>
2020-10-20 11:16:12 +0900

    Add: layout 작성 - 전반적인 layout 완성함

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,93 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Nav.scss";
 
-export class Nav extends Component {
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <nav className="Nav">
+        <div className="lowerSide">
+          <div className="container">
+            <div className="logoWrap">
+              <img src="./images/logo.png" alt="logo" className="logo" />
+            </div>
+            <div className="navContainer">
+              <div className="topLineWrap">
+                <form className="searchBarWrap">
+                  <input type="text" className="searchBar" />
+                  <button className="searchBarBtn">
+                    <FontAwesomeIcon
+                      classname="searchBarIcon"
+                      icon={faSearch}
+                    />
+                  </button>
+                </form>
+                <div className="mediaIconsWrap">
+                  <ul className="mediaIcons">
+                    <li className="facebook">
+                      <a href="https://www.facebook.com/">
+                        <FontAwesomeIcon icon={faFacebook} />
+                      </a>
+                    </li>
+                    <li className="instagram">
+                      <a href="https://www.instagram.com/">
+                        <FontAwesomeIcon icon={faInstagram} />
+                      </a>
+                    </li>
+                    <li className="naver"></li>
+                    <li className="branch"></li>
+                    <li className="youtube">
+                      <a href="https://www.youtube.com/">
+                        <FontAwesomeIcon icon={faYoutube} />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="loginWrap">
+                  <Link className="loginLink" to="/login">
+                    LOGIN
+                  </Link>
+                  <span className="loginAnd">or</span>
+                  <Link className="signupLink" to="/signup">
+                    REGISTER
+                  </Link>
+                </div>
+              </div>
+              <ul className="navbarList">
+                <li className="careby">
+                  <span className="carebyLink">CARE BY</span>
+                </li>
+                <li className="magazine">
+                  <span className="magazineLink">MAGAZINE</span>
+                </li>
+                <li className="pick">
+                  <Link to="/pick" className="pickLink">
+                    PICK
+                  </Link>
+                </li>
+                <li className="promotion">
+                  <span className="promotionLink">PROMOTION</span>
+                </li>
+                <li className="preOrder">
+                  <span className="preOrderLink">PRE-ORDER</span>
+                </li>
+                <li className="booking">
+                  <span className="bookingLink">BOOKING</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 
