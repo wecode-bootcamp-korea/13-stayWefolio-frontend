f42467f664073359d49870a95c02fe065752f1c1
JiyeonMin <ichbinmin2@gmail.com>
2020-10-21 11:25:31 +0900

    Add : 로그인 페이지 레이아웃 - 로그인 페이지 레이아웃 구현

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 import "./index.css";
 import "./styles/common.scss";
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
+import "../Login/Login.scss";
 
 export class Login extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="loginWrap">
+        <div className="navWrap">
+          <nav></nav>
+        </div>
+        <div className="loginContainer">
+          <div className="navWrap">
+            <nav></nav>
+          </div>
+          <div className="loginBoard">
+            <header>
+              <span>Login</span>
+            </header>
+            <section className="btnSection">
+              <button className="naverLogin">Login with Naver</button>
+              <button className="facebookLogin">Login with Facebook</button>
+              <button className="guestLogin">Guest Booking</button>
+            </section>
+            <section className="inputSection">
+              <form>
+                <input
+                  className="emailInput"
+                  placeholder="Email Address"
+                ></input>
+                <input className="passwordInput" placeholder="Password"></input>
+                <button className="loginBtn">Login</button>
+              </form>
+            </section>
+            <section className="signupSection">
+              <div className="signupBtnWrap">
+                <button className="signupBtn">Sign up</button>
+              </div>
+              <span className="blockLine">|</span>
+              <div className="searchPassBtnWrap">
+                <button className="searchPassBtn">Forgot password</button>
+              </div>
+            </section>
+          </div>
+        </div>
+        <div className="footerWrap">
+          <footer></footer>
+        </div>
+      </div>
+    );
   }
 }
 
