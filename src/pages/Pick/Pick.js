664a2fcc8fe7e1b1f30d9e67c40dc08077f90bc5
eunsunShin <unusual0218@gmail.com>
2020-10-21 17:37:13 +0900

    Add: mock data 추가 Refactor: mock data와map method를 이용한 component 재사용

--- src/pages/Pick/Pick.js
+++ src/pages/Pick/Pick.js
@@ -1,34 +1,104 @@
 import React, { Component } from "react";
-
-import "./Pick.scss"
 import PickArticle from "./PickArticle/PickArticle";
 
+import "./Pick.scss"
+
+
 export class Pick extends Component {
+  constructor () {
+    super();
+    this.state = {
+      hotelData : [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/data/pick_data.json', {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          hotelData: res.pick_data
+        });
+      });
+  }
+
   render() {
+    const { hotelData } = this.state;
     return (
       <div className="Pick">
         <div className="container">
-          {/*<div className="styleLine"></div>*/}
-
 
           <section>
-            <div>
-              <span>PICK</span><br></br>
-              <span>매일 하루 한번! 스테이폴리오가 추천합니다</span>  
+            <div className="titleInfo">
+              <span className="title">PICK</span><br></br>
+              <span className="titleInfo">매일 하루 한번! 스테이폴리오가 추천합니다</span>  
             </div>
             <div className="searchContainer">
-              <div className="searchType"></div>
-              <div className="searchLocation"></div>
-              <div className="searchPrice"></div>
+              <div className="searchfilter">
+                <span>타입전체</span>
+                <div className="point"></div>
+              </div>
+              <div className="searchfilter">
+                <span>지역전체</span>
+              </div>
+              <div className="searchfilter">
+                <span>금액전체</span>
+              </div>
             </div>
           </section>
           <div className="articleCon">
-            <PickArticle/>
-            <PickArticle/>
-            <PickArticle/>
-            <PickArticle/>
-            <PickArticle/>
-            <PickArticle/>
+            {hotelData.map((hotel) => (
+              <PickArticle 
+                name={hotel.name} 
+                engName={hotel.engName} 
+                desc={hotel.desc} 
+                mainImg={hotel.mainImg} 
+                location={hotel.location} 
+                type={hotel.type} 
+                minPrice={hotel.minPrice} 
+                maxPrice={hotel.maxPrice} 
+                stars={hotel.stars} 
+              />
+            ))}
+          </div>
+          <div className="pagination">
+            <ul>
+              <li>
+                <a href="/">&#60;&#60;</a>
+              </li>
+              <li>
+                <a href="/">&#60;</a>
+              </li>
+              <li>
+                <a href="/">1</a>
+              </li>
+              <li>
+                <a href="/">2</a>
+              </li>
+              <li>
+                <a href="/">3</a>
+              </li>
+              <li>
+                <a href="/">4</a>
+              </li>
+              <li>
+                <a href="/">5</a>
+              </li>
+              <li>
+                <a href="/">6</a>
+              </li>
+              <li>
+                <a href="/">7</a>
+              </li>
+              <li>
+                <a href="/">&#62;</a>
+              </li>
+              <li>
+                <a href="/">&#62;&#62;</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- src/pages/Pick/PickArticle/PickArticle.js
+++ src/pages/Pick/PickArticle/PickArticle.js
@@ -7,45 +7,45 @@ import "./PickArticle.scss"
 
 export class PickArticle extends Component {
   render() {
+    const { name, engName, desc, mainImg, location, type, minPrice, maxPrice, stars} = this.props;
+    console.log(stars)
     return (
       <article className="PickArticle">
         <a href="/">
-        <img alt="mainimg" src="https://s3.ap-northeast-2.amazonaws.com/stayfolio.images/system/pictures/images/000/056/218/medium/5aede287d43e0fa248825c78fd3cf1ae5880d4cd.jpg?1602836882" />
+        <img alt="mainimg" src={mainImg} />
         </a>
         <div className="articleInfo">
           <div className="hotelName">
-            <h4><a href>메이슨 파타야</a></h4>
-            <span className="eng">Mason Pattaya</span>
+            <h4><a href>{name}</a></h4>
+            <span className="eng">{engName}</span>
           </div>
           <div className="bookingLink">
             <a href="/">BOOKING NOW</a>
           </div>
-          <span>조용한 휴식이 필요하다면 찾게 되는 곳</span>
+          <span>{desc}</span>
     
           <div className="infoCon">
-
-
             <div className="infoLine">
               <div className="con">
                 <i className="fas fa-map-marker-alt"></i>
-                <span>태국/파타야</span>
+                <span>{location}</span>
                 <span className="centerLine">|</span>
               </div>
               <div className="con">
                 <i class="fas fa-home"></i>
-                <span>리조트</span>  
+                <span>{type}</span>  
               </div>
             </div>
 
             <div className="infoLine">
               <div className="con">
                 <i class="fas fa-coins"></i>
-                <span>532,000~2,139,000</span>
+                <span>{minPrice}~{maxPrice}</span>
                 <span className="centerLine">|</span>
               </div>
               <div className="con">
                 <i class="fas fa-star"></i>
-                <span>휴식, 해외여행, 수영장</span>  
+                <span>{stars.map((star)=>(star))}</span>  
               </div>
             </div>
           </div>
