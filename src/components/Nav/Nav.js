24186513ec0516edcdea39a58a7379db06b54a3f
suyeon <66218824+suyeon-DV@users.noreply.github.com>
2020-10-23 20:02:23 +0900

    Refactor: 멘토 리뷰 반영 - mock data 따로 분리 - mixin을 변수 이용에서 사용하도록 변경 - 스타일 적용되지 않는 nesting 삭제 - width, height 100% 모두 삭제 후 각 요소가 공간을 차지하도록 수정 - 공통된 스타일이 적용되는 className 삭제 후 하나의 Tag만을 사용하도록 수정 - className의 동적사용에 따라 스타일이 변경되도록 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,55 +2,30 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.scss";
 
-const brandLogos = [
-  {
-    id: "1",
-    href: "https://www.facebook.com/",
-    src: "./images/facebookimg.png",
-    alt: "facebooklogo",
-  },
-  {
-    id: "2",
-    href: "https://www.instagram.com/",
-    src: "./images/instaimg.png",
-    alt: "instagramlogo",
-  },
-  {
-    id: "3",
-    href: "https://www.naver.com/",
-    src: "./images/naverimg.png",
-    alt: "naverlogo",
-  },
-  {
-    id: "4",
-    href: "https://brunch.co.kr/",
-    src: "./images/branchimg.png",
-    alt: "branchlogo",
-  },
-  {
-    id: "5",
-    href: "https://www.youtube.com/",
-    src: "./images/youtubeimg.png",
-    alt: "youtubelogo",
-  },
-];
-
-const menus = [
-  { id: "1", path: "/careby", classname: "careByLink", menu: "CARE BY" },
-  { id: "2", path: "/magazine", classname: "magazineLink", menu: "MAGAZINE" },
-  { id: "3", path: "/pick", classname: "pickLink", menu: "PICK" },
-  {
-    id: "4",
-    path: "/promotion",
-    classname: "promotionLink",
-    menu: "PROMOTION",
-  },
-  { id: "5", path: "/preorder", classname: "preorderLink", menu: "PRE-ORDER" },
-  { id: "6", path: "/booking", classname: "bookingLink", menu: "BOOKING" },
-];
+const API = "http://localhost:3000/data/dataSSY/navdata.json";
 
 class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      brandLogos: [],
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          brandLogos: res.brandLogos,
+          menus: res.menus,
+        });
+      });
+  }
+
   render() {
+    const { brandLogos, menus } = this.state;
     return (
       <nav className="Nav">
         <div className="container">
@@ -92,7 +67,14 @@ class Nav extends Component {
               {menus.map((menu) => {
                 return (
                   <li key={menu.id}>
-                    <Link path={menu.path} className={menu.classname}>
+                    <Link
+                      path={menu.path}
+                      className={
+                        menu.menu.includes("BOOKING")
+                          ? "menuLink booking"
+                          : "menuLink"
+                      }
+                    >
                       {menu.menu}
                     </Link>
                   </li>
