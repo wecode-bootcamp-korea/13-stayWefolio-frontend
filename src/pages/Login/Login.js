7bfc117b7d7b748804b7d3591f3e181983e944f3
JiyeonMin <ichbinmin2@gmail.com>
2020-10-25 19:11:29 +0900

    Add : input 창 조건식에 따른 버튼 활성화 input 창 조건식에 따른 버튼 활성화

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
 import "../Login/Login.scss";
+import { withRouter } from "react-router-dom";
 
 export class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "@",
+      name: "ab",
+      password: "12345678",
+    };
+  }
+
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // goToMain = () => {
+  //   this.props.history.push("/");
+  // };
+
+  goToSignUp = () => {
+    this.props.history.push("/signup");
+  };
+  // handleLogin = (e) => {
+  //   e.preventDefault();
+  //   fetch(API, {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       email: this.state.email,
+  //       password: this.state.password,
+  //     }),
+  //   })
+  //     // 비동기 처리
+  //     .then((response) => response.json())
+  //     .then((result) => console.log("결과: ", result));
+  //   // .then((result) => {
+  //   //   console.log("=============");
+  //   //   console.log("백엔드 응답 메세지 :", result);
+  //   //   if (result.MESSAGE === "SUCCESS") {
+  //   //     localStorage.setItem("token", result);
+  //   // this.props.history.push("/");
+  //   //   }
+  //   // });
+  // };
+
   render() {
     return (
       <div className="loginWrap">
@@ -24,16 +70,58 @@ export class Login extends Component {
             <section className="inputSection">
               <form>
                 <input
+                  name="email"
+                  onChange={this.handleInput}
+                  type="text"
                   className="emailInput"
+                  required
                   placeholder="Email Address"
                 ></input>
-                <input className="passwordInput" placeholder="Password"></input>
-                <button className="loginBtn">Login</button>
+                <span
+                  name="inputSign"
+                  className={
+                    this.state.email.includes("@")
+                      ? "emailMessage"
+                      : "warningMessage"
+                  }
+                >
+                  이메일 주소를 입력하세요.
+                </span>
+                <input
+                  name="password"
+                  onChange={this.handleInput}
+                  type="password"
+                  className="passwordInput"
+                  required
+                  placeholder="Password"
+                ></input>
+                <span
+                  className={
+                    this.state.password.length > 7
+                      ? "passwordMessage "
+                      : "warningMessage"
+                  }
+                >
+                  비밀번호를 입력하세요.
+                </span>
+                <button
+                  onClick={this.handleLogin}
+                  className={
+                    this.state.email.includes("@") &&
+                    this.state.password.length > 7
+                      ? "loginBtn falseBtn"
+                      : "falseBtn"
+                  }
+                >
+                  Login
+                </button>
               </form>
             </section>
             <section className="signupSection">
               <div className="signupBtnWrap">
-                <button className="signupBtn">Sign up</button>
+                <button onClick={this.goToSignUp} className="signupBtn">
+                  Sign up
+                </button>
               </div>
               <span className="blockLine">|</span>
               <div className="searchPassBtnWrap">
@@ -50,4 +138,4 @@ export class Login extends Component {
   }
 }
 
-export default Login;
+export default withRouter(Login);
