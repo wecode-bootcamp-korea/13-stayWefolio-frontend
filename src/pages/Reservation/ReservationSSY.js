a960342fd7e15be7565f09b552bff2f4ee19ca82
suyeon <66218824+suyeon-DV@users.noreply.github.com>
2020-10-27 22:26:42 +0900

    Add: input 값 관리하는 함수 추가 - input 값 가져오는 함수 추가

--- src/pages/Reservation/ReservationSSY.js
+++ src/pages/Reservation/ReservationSSY.js
@@ -1,9 +1,54 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 import "./ReservationSSY.scss";
 
+const OPTIONS = [
+  { id: 1, value: 0, description: "0명" },
+  { id: 2, value: 1, description: "1명" },
+  { id: 3, value: 2, description: "2명" },
+];
+
 class Reservation extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      phoneNumber: "",
+      email: "",
+      adult: 0,
+      child: 0,
+      infant: 0,
+      optionBreakfast: false,
+      optionPickUp: false,
+      demand: "",
+      price: 0,
+      discount: 0,
+      total: 0,
+      paymentId: 0,
+    };
+  }
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleOption = (event) => {
+    const { name } = event.target;
+    const isChecked = event.currentTarget.value === "true" ? true : false;
+    this.setState({ [name]: isChecked });
+  };
+
+  handlePayTool = (event) => {
+    const { name } = event.target;
+    const isPayment = event.currentTarget.value === "naver" ? 1 : 2;
+
+    this.setState({ [name]: isPayment });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="Reservation">
         <div className="container">
@@ -34,78 +79,134 @@ class Reservation extends React.Component {
                       <span className="checkOutDate"> - (Check out Date)</span>
                     </div>
                   </div>
+
                   <div className="name">
                     <span className="title">이름</span>
-                    <input type="text"></input>
+                    <input
+                      type="text"
+                      name="name"
+                      onChange={this.handleInput}
+                    ></input>
                   </div>
+
                   <div className="contactNum">
                     <span className="title">연락처</span>
-                    <input type="number"></input>
+                    <input
+                      type="text"
+                      name="phoneNumber"
+                      onChange={this.handleInput}
+                    ></input>
                   </div>
+
                   <div className="email">
                     <span className="title">이메일</span>
-                    <input type="text"></input>
+                    <input
+                      type="text"
+                      name="email"
+                      onChange={this.handleInput}
+                    ></input>
                   </div>
+
                   <div className="people">
                     <span className="title">인원(최대 2명)</span>
                     <div className="selectWrap">
                       <div className="adultWrap">
                         <span>성인</span>
-                        <select name="adult" className="adult">
-                          <option value="0명">0명</option>
-                          <option value="1명">1명</option>
-                          <option value="2명">2명</option>
+                        <select
+                          name="adult"
+                          className="adult"
+                          onChange={this.handleInput}
+                        >
+                          {OPTIONS.map((option) => {
+                            return (
+                              <option key={option.id} value={option.value}>
+                                {option.description}
+                              </option>
+                            );
+                          })}
                         </select>
                       </div>
+
                       <div className="childWrap">
                         <span>소아</span>
-                        <select name="child" className="child">
-                          <option value="0명">0명</option>
-                          <option value="1명">1명</option>
-                          <option value="2명">2명</option>
+                        <select
+                          name="child"
+                          className="child"
+                          onChange={this.handleInput}
+                        >
+                          {OPTIONS.map((option) => {
+                            return (
+                              <option key={option.id} value={option.value}>
+                                {option.description}
+                              </option>
+                            );
+                          })}
                         </select>
                       </div>
-                      <div className="toddlerWrap">
+                      <div className="infantWrap">
                         <span
-                          className="toddler
+                          className="infant
                         "
                         >
                           유아
                         </span>
-                        <select name="toddler" className="toddler">
-                          <option value="0명">0명</option>
-                          <option value="1명">1명</option>
-                          <option value="2명">2명</option>
+                        <select
+                          name="infant"
+                          className="infant"
+                          onChange={this.handleInput}
+                        >
+                          {OPTIONS.map((option) => {
+                            return (
+                              <option key={option.id} value={option.value}>
+                                {option.description}
+                              </option>
+                            );
+                          })}
                         </select>
                       </div>
                     </div>
                   </div>
+
                   <div className="option">
-                    <span className="title">추가/옵션 선택</span>
+                    <span className="optionTitle">추가/옵션 선택</span>
                     <div className="miniOption">
                       <div className="breakfastWrap">
                         <div className="breakfastTitleWrap">
-                          <span className="title">조식</span>
+                          <span className="breackfastTitle">조식</span>
                           <span className="breakfastPrice">5,000원</span>
                         </div>
                         <p>
-                          1인당 추가 7,000원입니다. 인원과 일 수에 맞게
-                          입력해주세요. 건강한 호밀빵, 무농약 샐러드, 제철채소와
-                          과일, 무항생제 제주소세지로 구성되었습니다.
+                          1인당 추가 5,000원입니다. 건강한 호밀빵, 무농약
+                          샐러드, 제철채소와 과일, 무항생제 제주소세지로
+                          구성되었습니다.
                         </p>
-                        <div className="countWrap">
-                          <button className="minus" type="button">
-                            <i class="fas fa-minus"></i>
-                          </button>
-                          <span>0</span>
-                          <button className="plus" type="button">
-                            <i class="fas fa-plus"></i>
-                          </button>
+                        <div className="breakfastCheckBoxs">
+                          (필수)
+                          <label className="check" htmlFor="check">
+                            <input
+                              type="radio"
+                              value="true"
+                              name="optionBreakfast"
+                              onChange={this.handleOption}
+                            />
+                            선택
+                          </label>
+                          <label className="check" htmlFor="uncheck">
+                            <input
+                              type="radio"
+                              value="false"
+                              name="optionBreakfast"
+                              onChange={this.handleOption}
+                            />
+                            선택하지 않음
+                          </label>
                         </div>
                       </div>
                       <div className="pickUpWrap">
                         <div className="pickUpTitleWrap">
-                          <span className="title">픽업 서비스(필수선택)</span>
+                          <span className="pickUpTitle">
+                            픽업 서비스(필수선택)
+                          </span>
                         </div>
                         <p>
                           - 하루 2회 서울 시내권 픽업차량이 순회중이오니,
@@ -116,55 +217,94 @@ class Reservation extends React.Component {
                         <div className="pickUpCheckBoxs">
                           (필수)
                           <label className="check" htmlFor="check">
-                            <input type="radio" />
+                            <input
+                              type="radio"
+                              value="true"
+                              name="optionPickUp"
+                              onChange={this.handleOption}
+                            />
                             선택
                           </label>
                           <label className="check" htmlFor="uncheck">
-                            <input type="radio" />
+                            <input
+                              type="radio"
+                              value="false"
+                              name="optionPickUp"
+                              onChange={this.handleOption}
+                            />
                             선택하지 않음
                           </label>
                         </div>
                       </div>
                     </div>
                   </div>
+
                   <div className="request">
                     <span className="title">요청사항</span>
-                    <textarea></textarea>
+                    <textarea
+                      name="demand"
+                      onChange={this.handleInput}
+                    ></textarea>
                   </div>
 
                   <div className="price">
                     <span className="title">Total</span>
-
                     <div className="totalDetail">
                       <div className="roomPrice">
                         <span className="priceTitle">객실요금</span>
-                        <span className="price">-</span>
-                        <span className="won">원</span>
+                        <div className="priceWrap">
+                          <span className="priceCal">2,070,000</span>
+                          <span className="won">원</span>
+                        </div>
                       </div>
 
                       <div className="optionPrice">
                         <span className="priceTitle">추가옵션</span>
-                        <span className="price">-</span>
-                        <span className="won">원</span>
+                        <div className="priceWrap">
+                          <span className="priceCal">-207,000</span>
+                          <span className="won">원</span>
+                        </div>
                       </div>
 
                       <div className="discountPrice">
                         <span className="priceTitle">할인금액</span>
-                        <span className="price">-</span>
-                        <span className="won">원</span>
+                        <div className="priceWrap">
+                          <span className="priceCal">-</span>
+                          <span className="won">원</span>
+                        </div>
                       </div>
 
                       <div className="totalPrice">
                         <span className="priceTitle">총 결제금액</span>
-                        <span className="price">-</span>
-                        <span className="won">원</span>
+                        <div className="totalPriceWrap">
+                          <span className="priceCal">1,863,000</span>
+                          <span className="won">원</span>
+                        </div>
                       </div>
                     </div>
                   </div>
+
                   <div className="payTool">
                     <span className="title">결제정보</span>
-                    <div className="toolOption">
-                      네이버페이, 신용카드, 페이팔
+                    <div className="payOptions">
+                      <label htmlFor="naverPay">
+                        <input
+                          type="radio"
+                          name="paymentId"
+                          value="naver"
+                          onChange={this.handlePayTool}
+                        />
+                        네이버페이
+                      </label>
+                      <label htmlFor="creditCard">
+                        <input
+                          type="radio"
+                          name="paymentId"
+                          value="creditCard"
+                          onChange={this.handlePayTool}
+                        />
+                        신용카드
+                      </label>
                     </div>
                   </div>
                 </div>
