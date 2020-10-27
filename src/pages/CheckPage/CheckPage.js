706db47acea02e340ad90f7ddcc5ee583189ba69
Tania <tania@gimbolaui-MacBookPro-5.local>
2020-10-27 22:48:22 +0900

    Add: 예약 확인 페이지 및 데이터 fetch 완료

--- src/pages/CheckPage/CheckPage.js
+++ src/pages/CheckPage/CheckPage.js
@@ -1,38 +1,80 @@
 import React, { Component } from "react";
 import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav/Nav";
+import { withRouter } from "react-router-dom";
 import "./CheckPage.scss";
 
+const API = "http://localhost:3000/data/checkPageData/bookingData.json";
+
 class CheckPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bookingInfo: [],
+      adultCount: 0,
+      childCount: 0,
+      InfantCount: 0,
+    };
+  }
+
+  goToMain = () => {
+    this.props.history.push("/");
+  };
+
+  componentDidMount() {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => {
+        const { booking_info } = res;
+
+        this.setState({
+          bookingInfo: booking_info,
+          adultCount: Number(booking_info[0].adult),
+          childCount: Number(booking_info[0].child),
+          infantCount: Number(booking_info[0].infant),
+        });
+      });
+  }
+
   render() {
+    const { bookingInfo, adultCount, childCount, infantCount } = this.state;
+    console.log(this.state.childCount);
+
     return (
       <section className="CheckPage">
         <Nav />
         <div
           className="checkContainer"
           style={{
-            backgroundImage:
-              "url(" +
-              "https://images.unsplash.com/photo-1540541338287-41700207dee6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" +
-              ")",
+            backgroundImage: `url(${bookingInfo[0]?.hotel_image})`,
           }}
         >
           <div className="darken"></div>
           <div className="checkContent">
             <p className="checkText">
-              <span className="bolder1">김보라</span> 님, 감사합니다.
+              <span className="bolder1">{bookingInfo[0]?.name}</span> 님,
+              감사합니다.
               <br />
               <br />
               <span className="bolder2">
-                Solitaire Bangkok, 그랜드 수페리어룸
-                <br /> 성인 2명 / 소아 1명 <br />
-                12월 13일 - 12월 20일
+                {bookingInfo[0]?.hotel_name}, {bookingInfo[0]?.room_name}
+                <br /> {adultCount !== 0 && `성인 ${adultCount}명 `}
+                {childCount !== 0 && `/ 아동 ${childCount}명 `}
+                {infantCount !== 0 && `/ 유아 ${infantCount}명 `}
+                <br />
+                {bookingInfo[0]?.date_from.slice(0, 4)}년{" "}
+                {bookingInfo[0]?.date_from.slice(5, 7)}월{" "}
+                {bookingInfo[0]?.date_from.slice(8, 10)}일 -{" "}
+                {bookingInfo[0]?.date_to.slice(5, 7)}월{" "}
+                {bookingInfo[0]?.date_to.slice(8, 10)}일
                 <br />
                 <br />
               </span>{" "}
               예약이 완료되었습니다.
             </p>
-            <button className="goToMainBtn">MAIN으로 이동</button>
+            <button className="goToMainBtn" onClick={this.goToMain}>
+              MAIN으로 이동
+            </button>
           </div>
         </div>
         <Footer />
